367fff93d951af286c71a5760b17223762b75868
alfon-bujan <abujan@udesa.edu.ar>
2026-04-15 12:11:44 -0300

    carga_datos manejo de error

--- src/carga_datos.py
+++ src/carga_datos.py
@@ -9,20 +9,25 @@ def parsear_linea(linea):
     Retorno: diccionario con los datos del registro
 
     """
-    separo= linea.strip.split(",")
+    try:
+        separo= linea.strip.split(",")
     
-    registro = { 
-        "id_participante": int(separo[0]), 
-        "trial": int(separo[1]), 
-        "estimulo": str(separo[2]), 
-        "t_inicio": float(separo[3]), 
-        "respuesta": bool(separo[4]), 
-        "tiempo_reaccion": float(separo[5]), 
-        "resultado_respuesta": str(separo[6]), 
-        "condicion": str(separo[7]) 
-        } 
+        registro = { 
+                   "id_participante": int(separo[0]), 
+                   "trial": int(separo[1]), 
+                   "estimulo": str(separo[2]), 
+                   "t_inicio": float(separo[3]), 
+                   "respuesta": bool(separo[4]), 
+                   "tiempo_reaccion": float(separo[5]), 
+                   "resultado_respuesta": str(separo[6]), 
+                   "condicion": str(separo[7]) 
+                     } 
     
-    return registro
-
-
-
+        return registro
+    except IndexError:
+        print("Error en parsear_linea: la linea tiene menos columnas de las esperadas.")
+        return None
+    except ValueError:
+        print("Error en parsear_linea: un campo no pudo convertirse al tipo de dato esperado.")
+        return None
+ 
