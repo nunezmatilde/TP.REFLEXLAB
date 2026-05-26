82919e0e75322a4e5ae7f73493e24ec7204d1d95
nunezmatilde <nunezm@udesa.edu.ar>
2026-05-26 19:26:22 -0300

    Update carga_datos.py
    Aplicacion de pandas a la funcion leer_archivo dentro de carga datos. La cual se encarga de leer el CSV y organizar todo en filas y columnas. Termina convirtiendo todo a lista de diccionarios.

--- src/carga_datos.py
+++ src/carga_datos.py
@@ -1,12 +1,31 @@
 
+import pandas as pd
 
+def leer_archivo(ruta_archivo):
+    """
+    descripcion: lee un archivo CSV usando pandas y devuelve los datos como lista de diccionarios.
+    parametros: ruta_archivo (str)- ruta del archivo CSV.
+    retorno: list - lista de registros (diccionarios)
+    
+    """
+    try: 
+        df= pd.read_csv(ruta_archivo)
 
+        #convierte cada fila en un diccionario
+        datos= df.to_dict(orient="records") #cada fila un diccionario 
+        return datos 
+    
+    except Exception as e:
+        print(f"Error al leer el archivo con pandas: {e}")
+        return[]
+    
+    
 def parsear_linea(linea):
     """
     
-    Descripcion: convierte una linea de un archivo en un diccionario con los datos ordenados 
+    Descripcion: convierte una linea de un archivo en un diccionario con los datos ordenados  
     Parametros: linea(str), una linea de un archivo que tiene sus valores separados por una coma.
-    Retorno: diccionario con los datos del registro
+    Retorno: diccionario con los datos del registro o None si salta un error
 
     """
     try:
@@ -25,12 +44,12 @@ def parsear_linea(linea):
             "tiempo_reaccion": float(separo[5]), 
             "resultado_respuesta": str(separo[6]), 
             "condicion": str(separo[7]) 
-            } 
+            }
      
         return registro
 
     except Exception as e:
-        print(f"Error al parafrasear {e}")
+        print(f"Error: {e}")
        
         return None
 
