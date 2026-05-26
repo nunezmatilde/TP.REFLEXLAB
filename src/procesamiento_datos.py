898941c7a674a9597eab5f40cc906fc3c3e10f2d
nunezmatilde <nunezm@udesa.edu.ar>
2026-05-26 19:44:49 -0300

    Update procesamiento_datos.py
    Le aplique pandas a la funcion filtrar_por_participante en donde recorro y filtro uno por uno  y se van agarrando los registros y se calculan las metricas individuales.

--- src/procesamiento_datos.py
+++ src/procesamiento_datos.py
@@ -1,20 +1,26 @@
+import pandas as pd
+
 
 #datos seria una lista de diccionarios 
 
-def filtrar_por_participante(datos, id_participante):
+def filtrar_por_participante(datos, id_participante= None):
     """
     
-    Descripcion: analiza los datos y devuelve los datos del participante solicitado
+    Descripcion: filtra los datos y devuelve los datos del participante solicitado o devuelve los IDs disponibles utilizando pandas 
     Parametro: datos(lista), es una lista de diccionarios con los registros
                 id_participante(id), es para identificar al participante a analizar
-    Retorno: devuelve una lista con la informacion del participante solicidato
+    Retorno: list- devuelve una lista de registros con la informacion del participante solicidato o lista de IDs si no se especifica el participante. 
 
     """
-    resultado=[] #creo la lista vacia
+    #convertir lista de diccionarios a DataFrame
+    df= pd.DataFrame(datos)
     
-    for dato in datos: #recorro la info
-        ID= dato["id_participante"]
-        if ID == id_participante: #si coincide el id se agrega a la lista creada anteriormente
-            resultado.append(dato)
+    if id_participante is None:
+        return df["id_participante"].unique().tolist()
+    
+    #filtrar por participantes usando pandas
+    resultado= df[df["id_participante"] == id_participante]
+   
+    #convierto devuelta a la lista de diccionarios 
             
-    return resultado #una vez finalizado se retorna la lista
+    return resultado.to_dict(orient="records")#una vez finalizado se retorna la lista
